da26ccc051caac9d1adae7a3fd5e1995ceeb7a36
lawalsegun2025 <lawalsegun2025@gmail.com>
2023-05-19 16:49:24 +0100

    modified

--- portfolio/models.py
+++ portfolio/models.py
@@ -4,3 +4,4 @@ class Project(models.Model):
     title=models.CharField(max_length=100)
     description=models.CharField(max_length=300)
     image=models.ImageField(upload_to='portfolio/images')
+    url=models.URLField(blank=True)
